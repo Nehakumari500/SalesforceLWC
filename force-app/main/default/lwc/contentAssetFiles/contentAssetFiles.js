5b92c7d19181d4985bc240a697aab16fc0f9ab72
Neha Kumari <nehakumari500@gmail.com>
2023-08-21 22:07:01 +0530

    Content asset file add , use and download

---
+++ force-app/main/default/lwc/contentAssetFiles/contentAssetFiles.js
@@ -0,0 +1,5 @@
+import { LightningElement } from 'lwc';
+import TYPESCRIPT_FILE from '@salesforce/contentAssetUrl/JavaScript_Developer_I_Cheat_Sheetpdf'
+export default class ContentAssetFiles extends LightningElement {
+    file = TYPESCRIPT_FILE
+}
